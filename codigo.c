80556a09d060ba0257c2f59dca3b89bf5e5d0126
pbettancourt2022 <pbettancourt2022@udec.cl>
2023-12-01 16:29:31 -0300

    avance dijkstra

--- codigo.c
+++ codigo.c
@@ -5,6 +5,7 @@
 #include <stdbool.h>
 
 #define MAX_INTERSECCIONES 100
+#define INF 9999999 // Valor infinito para representar distancias desconocidas
 
 typedef struct {
     char nombre[50];
@@ -65,6 +66,7 @@ void leerConexionesDesdeArchivo(GrafoDirigido *grafo, const char *nombreArchivo)
         exit(EXIT_FAILURE);
     }
 
+
     char interseccion1[50], interseccion2[50];
     int distancia;
 
@@ -84,9 +86,66 @@ void leerConexionesDesdeArchivo(GrafoDirigido *grafo, const char *nombreArchivo)
 }
 
 void dijkstra(GrafoDirigido *grafo, int inicio, int fin) {
-    // Implementación de Dijkstra
-    // ...
+    int distancias[MAX_INTERSECCIONES]; // Almacena las distancias más cortas
+    bool visitado[MAX_INTERSECCIONES]; // Marca los nodos visitados
+    int padres[MAX_INTERSECCIONES]; // Almacena los nodos padres
+
+    // Inicializar distancias y nodos visitados
+    for (int i = 0; i < grafo->numIntersecciones; ++i) {
+        distancias[i] = INF;
+        visitado[i] = false;
+        padres[i] = -1; // Valor nulo para los nodos padres
+    }
+
+    // La distancia al nodo de inicio es 0
+    distancias[inicio] = 0;
+
+    for (int count = 0; count < grafo->numIntersecciones - 1; ++count) {
+        // Encontrar el nodo con la distancia más corta aún no visitado
+        int u, min_distancia = INF;
+        for (int v = 0; v < grafo->numIntersecciones; ++v) {
+            if (visitado[v] == false && distancias[v] <= min_distancia) {
+                min_distancia = distancias[v];
+                u = v;
+            }
+        }
+
+        // Marcar el nodo seleccionado como visitado
+        visitado[u] = true;
+
+        // Actualizar las distancias a los nodos adyacentes si son menores a través de u
+        for (int v = 0; v < grafo->numIntersecciones; ++v) {
+            if (!visitado[v] && grafo->conexiones[u][v].distancia &&
+                distancias[u] != INF && distancias[u] + grafo->conexiones[u][v].distancia < distancias[v]) {
+                distancias[v] = distancias[u] + grafo->conexiones[u][v].distancia;
+                padres[v] = u; // Actualizar el nodo padre
+            }
+        }
+    }
+
+    // Imprimir la ruta más corta si hay un camino desde el inicio al fin
+    if (distancias[fin] != INF) {
+        printf("La distancia más corta dessde %s hasta %s es: %d metros\n", grafo->intersecciones[inicio].nombre, grafo->intersecciones[fin].nombre, distancias[fin]);
+
+        printf("La ruta más corta es:\n");
+        int camino[MAX_INTERSECCIONES], longitud_camino = 0;
+        for (int i = fin; i != -1; i = padres[i]) {
+            camino[longitud_camino++] = i;
+        }
+
+        // Imprimir la ruta inversa (del fin al inicio)
+        for (int i = longitud_camino - 1; i >= 0; --i) {
+            printf("%s", grafo->intersecciones[camino[i]].nombre);
+            if (i != 0) {
+                printf(" -> ");
+            }
+        }
+        printf("\n");
+    } else {
+        printf("No hay ruta disponible desde %s hasta %s\n", grafo->intersecciones[inicio].nombre, grafo->intersecciones[fin].nombre);
+    }
 }
+
 
 void manejarCasosEspeciales(GrafoDirigido *grafo) {
     // Lógica específica para calles y direcciones del centro de Concepción
