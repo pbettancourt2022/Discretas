11965fdb3bffff39c26cf9b6900bcfedf5af706b
pbettancourt2022 <pbettancourt2022@udec.cl>
2023-12-04 12:00:47 -0300

    funciona el dijktra v.1

--- codigo.c
+++ codigo.c
@@ -4,203 +4,221 @@
 #include <limits.h>
 #include <stdbool.h>
 
-#define MAX_INTERSECCIONES 100
+#define MAX_INTERSECCIONES 112
 #define INF 9999999 // Valor infinito para representar distancias desconocidas
-
+#define V 112
 typedef struct {
     char nombre[50];
-    double latitud;
-    double longitud;
-} InterseccionCoordenadas;
+    int numero;
+} Interseccion;
 
 typedef struct {
     int distancia; // Distancia en metros
 } Conexion;
 
 typedef struct {
-    InterseccionCoordenadas intersecciones[MAX_INTERSECCIONES];
+    Interseccion intersecciones[MAX_INTERSECCIONES];
     Conexion conexiones[MAX_INTERSECCIONES][MAX_INTERSECCIONES];
     int numIntersecciones;
 } GrafoDirigido;
 
-void inicializarGrafo(GrafoDirigido *grafo) {
-    grafo->numIntersecciones = 0;
-}
+int matriz[V][V]; // Matriz de adyacencia para representar el grafo dirigido
+int distancia[V]; // Almacena la distancia más corta desde el vértice de origen
+int visitado[V]; // Indica si un vértice ha sido visitado o no
 
-void agregarInterseccion(GrafoDirigido *grafo, char nombre[], double latitud, double longitud) {
-    if (grafo->numIntersecciones < MAX_INTERSECCIONES) {
-        strcpy(grafo->intersecciones[grafo->numIntersecciones].nombre, nombre);
-        grafo->intersecciones[grafo->numIntersecciones].latitud = latitud;
-        grafo->intersecciones[grafo->numIntersecciones].longitud = longitud;
-        grafo->numIntersecciones++;
-    } else {
-        fprintf(stderr, "Demasiadas intersecciones. Ajusta MAX_INTERSECCIONES.\n");
-        exit(EXIT_FAILURE);
-    }
-}
+// Función para encontrar el vértice con la distancia mínima
+int minDistancia(int distancia[], int visitado[]) {
+    int min = INT_MAX, min_indice;
 
-void leerInterseccionesDesdeArchivo(GrafoDirigido *grafo, const char *nombreArchivo) {
-    FILE *archivo = fopen(nombreArchivo, "r");
-    if (archivo == NULL) {
-        perror("Error al abrir el archivo de intersecciones");
-        exit(EXIT_FAILURE);
-    }
-
-    char nombre[50];
-    double latitud, longitud;
-    while (fscanf(archivo, "%s %lf %lf", nombre, &latitud, &longitud) == 3) {
-        agregarInterseccion(grafo, nombre, latitud, longitud);
-    }
-
-    fclose(archivo);
-}
-
-void agregarConexion(GrafoDirigido *grafo, int indice1, int indice2, int distancia) {
-    grafo->conexiones[indice1][indice2].distancia = distancia;
-}
-
-void leerConexionesDesdeArchivo(GrafoDirigido *grafo, const char *nombreArchivo) {
-    FILE *archivo = fopen(nombreArchivo, "r");
-    if (archivo == NULL) {
-        perror("Error al abrir el archivo de conexiones");
-        exit(EXIT_FAILURE);
-    }
-
-
-    char interseccion1[50], interseccion2[50];
-    int distancia;
-
-    while (fscanf(archivo, "%s %s %d", interseccion1, interseccion2, &distancia) == 3) {
-        int indice1 = obtenerIndiceInterseccion(grafo, interseccion1);
-        int indice2 = obtenerIndiceInterseccion(grafo, interseccion2);
-
-        if (indice1 != -1 && indice2 != -1) {
-            agregarConexion(grafo, indice1, indice2, distancia);
-        } else {
-            fprintf(stderr, "Error: Intersecciones no encontradas.\n");
-            exit(EXIT_FAILURE);
+    for (int v = 0; v < V; v++) {
+        if (visitado[v] == 0 && distancia[v] <= min) {
+            min = distancia[v];
+            min_indice = v;
         }
     }
-
-    fclose(archivo);
+    return min_indice;
 }
 
-void dijkstra(GrafoDirigido *grafo, int inicio, int fin) {
-    int distancias[MAX_INTERSECCIONES]; // Almacena las distancias más cortas
-    bool visitado[MAX_INTERSECCIONES]; // Marca los nodos visitados
-    int padres[MAX_INTERSECCIONES]; // Almacena los nodos padres
+// Función para imprimir la solución (ruta más corta desde el vértice de origen)
+void imprimirSolucion(int distancia[], int origen) {
+    printf("Intersección \t Distancia desde el origen\n");
+    for (int i = 0; i < V; i++) {
+        printf("%d \t\t %d\n", i, distancia[i]);
+    }
+}
 
-    // Inicializar distancias y nodos visitados
-    for (int i = 0; i < grafo->numIntersecciones; ++i) {
-        distancias[i] = INF;
-        visitado[i] = false;
-        padres[i] = -1; // Valor nulo para los nodos padres
+// Algoritmo de Dijkstra para encontrar la ruta más corta
+void dijkstra(int matriz[V][V], int origen) {
+    for (int i = 0; i < V; i++) {
+        distancia[i] = INT_MAX;
+        visitado[i] = 0;
     }
 
-    // La distancia al nodo de inicio es 0
-    distancias[inicio] = 0;
+    distancia[origen] = 0;
 
-    for (int count = 0; count < grafo->numIntersecciones - 1; ++count) {
-        // Encontrar el nodo con la distancia más corta aún no visitado
-        int u, min_distancia = INF;
-        for (int v = 0; v < grafo->numIntersecciones; ++v) {
-            if (visitado[v] == false && distancias[v] <= min_distancia) {
-                min_distancia = distancias[v];
-                u = v;
-            }
-        }
+    for (int count = 0; count < V - 1; count++) {
+        int u = minDistancia(distancia, visitado);
+        visitado[u] = 1;
 
-        // Marcar el nodo seleccionado como visitado
-        visitado[u] = true;
-
-        // Actualizar las distancias a los nodos adyacentes si son menores a través de u
-        for (int v = 0; v < grafo->numIntersecciones; ++v) {
-            if (!visitado[v] && grafo->conexiones[u][v].distancia &&
-                distancias[u] != INF && distancias[u] + grafo->conexiones[u][v].distancia < distancias[v]) {
-                distancias[v] = distancias[u] + grafo->conexiones[u][v].distancia;
-                padres[v] = u; // Actualizar el nodo padre
+        for (int v = 0; v < V; v++) {
+            if (!visitado[v] && matriz[u][v] && distancia[u] != INT_MAX &&
+                distancia[u] + matriz[u][v] < distancia[v]) {
+                distancia[v] = distancia[u] + matriz[u][v];
             }
         }
     }
 
-    // Imprimir la ruta más corta si hay un camino desde el inicio al fin
-    if (distancias[fin] != INF) {
-        printf("La distancia más corta dessde %s hasta %s es: %d metros\n", grafo->intersecciones[inicio].nombre, grafo->intersecciones[fin].nombre, distancias[fin]);
-
-        printf("La ruta más corta es:\n");
-        int camino[MAX_INTERSECCIONES], longitud_camino = 0;
-        for (int i = fin; i != -1; i = padres[i]) {
-            camino[longitud_camino++] = i;
-        }
-
-        // Imprimir la ruta inversa (del fin al inicio)
-        for (int i = longitud_camino - 1; i >= 0; --i) {
-            printf("%s", grafo->intersecciones[camino[i]].nombre);
-            if (i != 0) {
-                printf(" -> ");
-            }
-        }
-        printf("\n");
-    } else {
-        printf("No hay ruta disponible desde %s hasta %s\n", grafo->intersecciones[inicio].nombre, grafo->intersecciones[fin].nombre);
-    }
+    imprimirSolucion(distancia, origen);
 }
 
-
-void manejarCasosEspeciales(GrafoDirigido *grafo) {
-    // Lógica específica para calles y direcciones del centro de Concepción
-    // ...
-}
-
-void imprimirRuta(GrafoDirigido *grafo, int *padres, int inicio, int fin) {
-    // Función para imprimir la ruta
-    // ...
-}
-
-void solicitarArchivo(char *nombreArchivo, const char *tipo) {
-    printf("Ingrese el nombre del archivo de %s: ", tipo);
-    scanf("%s", nombreArchivo);
-}
-
-int obtenerIndiceInterseccion(GrafoDirigido *grafo, char nombre[]) {
-    for (int i = 0; i < grafo->numIntersecciones; i++) {
-        if (strcmp(grafo->intersecciones[i].nombre, nombre) == 0) {
-            return i;
+int obtenerNumeroInterseccion(Interseccion intersecciones[], int numIntersecciones, char nombre[]) {
+    for (int i = 0; i < numIntersecciones; i++) {
+        if (strcmp(intersecciones[i].nombre, nombre) == 0) {
+            return intersecciones[i].numero;
         }
     }
-    return -1;
+    return -1; // Si no se encuentra la intersección
 }
 
 int main() {
     GrafoDirigido grafo;
-    inicializarGrafo(&grafo);
-
-    char nombreArchivoIntersecciones[50];
-    char nombreArchivoConexiones[50];
-    char inicio[50], fin[50];
-
-    solicitarArchivo(nombreArchivoIntersecciones, "intersecciones");
-    solicitarArchivo(nombreArchivoConexiones, "conexiones");
-
-    leerInterseccionesDesdeArchivo(&grafo, nombreArchivoIntersecciones);
-    leerConexionesDesdeArchivo(&grafo, nombreArchivoConexiones);
-
-    manejarCasosEspeciales(&grafo);
-
-    printf("Ingrese el punto de inicio: ");
-    scanf("%s", inicio);
-    printf("Ingrese el punto de fin: ");
-    scanf("%s", fin);
-
-    int indiceInicio = obtenerIndiceInterseccion(&grafo, inicio);
-    int indiceFin = obtenerIndiceInterseccion(&grafo, fin);
-
-    if (indiceInicio == -1 || indiceFin == -1) {
-        fprintf(stderr, "Error: Punto de inicio o fin no encontrado.\n");
-        exit(EXIT_FAILURE);
+    char entrada[50];
+    char origen[50];
+    char destino[50];
+    char paso[50];
+    int numOrigen, numDestino, numPaso;
+    FILE *archivo = fopen("intersecciones.txt", "r");
+    if (archivo == NULL) {
+        perror("Error al abrir el archivo de intersecciones");
+        return EXIT_FAILURE;
     }
 
-    dijkstra(&grafo, indiceInicio, indiceFin);
+    Interseccion intersecciones[MAX_INTERSECCIONES];
+    int numIntersecciones = 0;
+
+    char linea[100];
+    while (fgets(linea, sizeof(linea), archivo) != NULL) {
+        linea[strcspn(linea, "\n")] = 0; // Elimina el carácter de nueva línea
+
+        int numero = obtenerNumeroInterseccion(intersecciones, numIntersecciones, linea);
+        if (numero == -1) {
+            if (numIntersecciones < MAX_INTERSECCIONES) {
+                strcpy(intersecciones[numIntersecciones].nombre, linea);
+                intersecciones[numIntersecciones].numero = numIntersecciones;
+                numIntersecciones++;
+            } else {
+                fprintf(stderr, "Demasiadas intersecciones. Ajusta MAX_INTERSECCIONES.\n");
+                return EXIT_FAILURE;
+            }
+        }
+    }
+
+    fclose(archivo);
+    int matriz [112][112];
+    for (int j = 0; j < 111; j++){
+        if(j==0){
+            matriz[j][j+1]=1;
+        }
+        else if(((j>0)&&(j<13))&&(j%2!=0)){
+            matriz[j][j-1]=1;
+            matriz[j][j+1]=1;
+            matriz[j][j+14]=1;
+        }
+        else if ((j<13)&&(j%2==0)){
+            matriz[j][j-1]=1;
+            matriz[j][j+1]=1;
+        }
+        else if(j==13){
+            matriz[j][j-1]=1;
+            matriz[j][j+14]=1;
+        }
+        //Filas 14 42 70 -------
+        else if ((j==14)||(j==42)||(j==70)){
+            matriz[j][j+1]=1;
+            matriz[j][j-14]=1;
+        }
+        else if ((((j>14)&&(j<27))||((j>42)&&(j<55))||((j>70)&&(j<83)))&&(j%2!=0)&&(j!=21)&&(j!=49)&&(j!=81)){
+            matriz[j][j+1]=1;
+            matriz[j][j+14]=1;
+        }
+        else if ((j==21)){
+            matriz[j][j+1]=1;
+        }
+        else if ((j==49)){
+            matriz[j][j+14]=1;
+        }
+        else if ((j==81)){
+            matriz[j][j+1]=1;
+            matriz[j][j+14]=1;
+            matriz[j][66]=1;
+        }
+        else if ((((j>14)&&(j<27))||((j>42)&&(j<55))||((j>70)&&(j<83)))&&(j%2==0)&&(j!=50)){
+            matriz[j][j+1]=1;
+            matriz[j][j-14]=1;
+        }
+        else if (j==50){
+            matriz[j][j-14]=1;
+        }
+        else if ((j==27)||(j==55)){
+            matriz[j][j+14]=1;
+            matriz[j][j-14]=1;
+        }
+        else if (j==83){
+            matriz[j][j+14]=1;
+        }
+        //-----------
+        //Filas 28 56 84 -------
+        else if ((j==28)||(j==56)||(j==84)){
+            matriz[j][j-14]=1;
+        }
+        else if ((((j>28)&&(j<41))||((j>56)&&(j<69))||((j>84)&&(j<97)))&&(j%2!=0)&&(j!=35)){
+            matriz[j][j-1]=1;
+            matriz[j][j+14]=1;
+        }
+        else if (j==35){
+            matriz[j][j-1]=1;
+        }
+        else if ((((j>28)&&(j<41))||((j>56)&&(j<69))||((j>84)&&(j<97)))&&(j%2==0)&&(j!=96)){
+            matriz[j][j-1]=1;
+            matriz[j][j-14]=1;
+        }
+        else if (j==96){
+            matriz[j][j-1]=1;
+            matriz[j][j-14]=1;
+            matriz[j][81]=1;
+        }
+        else if ((j==41)||(j==69)){
+            matriz[j][j-1]=1;
+            matriz[j][j-14]=1;
+            matriz[j][j+14]=1;
+        }
+        else if (j==97){
+            matriz[j][j+14]=1;
+        }
+        //--------
+        else if (j==98){
+            matriz[j][j-14]=1;
+            matriz[j][j+1]=1;
+        }
+        else if(((j>98)&&(j<111))&&(j%2!=0)){
+            matriz[j][j-1]=1;
+            matriz[j][j+1]=1;
+        }
+        else if(((j>98)&&(j<111))&&(j%2==0)){
+            matriz[j][j-1]=1;
+            matriz[j][j+1]=1;
+            matriz[j][j-14]=1;
+        }
+        else if (j==111){
+            matriz[j][j-1]=1;
+            matriz[j][96]=1;
+        }
+    }
+    printf("Ingrese tres lugares y sus números separados por espacios entre comillas (Ejemplo: \"Orompello 100\" \"Rengo 400\" \"Paicavi 600\"): ");
+    fgets(entrada, sizeof(entrada), stdin);
+    sscanf(entrada, "\"%s %d\" \"%s %d\" \"%s %d\"", origen, &numOrigen, destino, &numDestino, paso, &numPaso);
+
+    dijkstra(matriz, 0);
 
     return 0;
 }
